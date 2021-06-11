602a731f218e25c1e632046bb708ea7b733cec64
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-06-11 13:04:02 -0300

    I'm now using the inertia-head at the app layouts to connect the fonts. It is apparently better...

--- resources/js/Layouts/AppPublic.vue
+++ resources/js/Layouts/AppPublic.vue
@@ -1,4 +1,7 @@
 <template>
+<inertia-head>
+            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Merriweather:wght@200;400;700;900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400&display=wrap">
+</inertia-head>
     <div id="_website_">
         <navigation />
         <messages :errors="$page.props.errors" :status="message"/>
